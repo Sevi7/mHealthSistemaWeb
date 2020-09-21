cfcc6294a694cdd3de12312f98b94ea22431cf31
Sevi7 <adrisevilla98@gmail.com>
2020-09-21 20:15:27 +0100

    eliminar todas las constantes vitales almacenadas del usuario cuando este elimina su cuenta

--- routes/usuarioRoutes.js
+++ routes/usuarioRoutes.js
@@ -2,35 +2,52 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 const Usuario = mongoose.model('Usuario');
+const MedicionConstanteVital = mongoose.model('MedicionConstanteVital');
 const routerUsuario = express.Router();
 const middleware = require('./middleware.js');
 
 routerUsuario.use(middleware.comprobarToken);
 
 routerUsuario.get('/', async (req, res) => {
-  const usuario = await Usuario.findById(req.usuarioId);
+  const { usuarioId } = req;
+  const usuario = await Usuario.findById(usuarioId);
 
+  const usuarioDatos = {
+    id: usuario.id,
+    nombre: usuario.nombre,
+    apellidos: usuario.apellidos,
+    email: usuario.email,
+    fechaNacimiento: usuario.fechaNacimiento,
+    altura: usuario.altura,
+    sexo: usuario.sexo,
+    diabetes: usuario.diabetes,
+  };
   return res.status(202).send({
     ok: true,
-    usuario,
-  });
-});
-
-routerUsuario.put('/', async (req, res) => {
-  const usuario = await Usuario.findByIdAndUpdate(req.usuarioId, req.body);
-
-  return res.status(202).send({
-    ok: true,
-    usuario,
+    usuarioDatos,
   });
 });
 
 routerUsuario.delete('/', async (req, res) => {
-  const usuario = await Usuario.findByIdAndDelete(req.usuarioId);
+  const { usuarioId } = req;
+  const usuario = await Usuario.findByIdAndDelete(usuarioId);
+
+  await MedicionConstanteVital.deleteMany({ usuario: usuarioId });
+
+  const usuarioDatos = {
+    id: usuario.id,
+    nombre: usuario.nombre,
+    apellidos: usuario.apellidos,
+    email: usuario.email,
+    fechaNacimiento: usuario.fechaNacimiento,
+    altura: usuario.altura,
+    sexo: usuario.sexo,
+    diabetes: usuario.diabetes,
+  };
 
   return res.status(202).send({
     ok: true,
-    usuario,
+    usuarioDatos,
   });
 });
 
